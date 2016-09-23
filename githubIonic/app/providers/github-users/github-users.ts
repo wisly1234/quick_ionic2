18bc6eb0afaf1fdbca25435cfc11f9085eadc382
wislylov <wislylou@firevale.com>
2016-09-23 09:35:00 +0800

    wip

--- githubIonic/app/providers/github-users/github-users.ts
+++ githubIonic/app/providers/github-users/github-users.ts
@@ -2,6 +2,10 @@ import { Injectable } from '@angular/core';
 import { Http } from '@angular/http';
 import 'rxjs/add/operator/map';
 
+
+// Import the user model
+import {User} from '../../models/user'
+
 /*
   Generated class for the GithubUsers provider.
 
@@ -10,8 +14,33 @@ import 'rxjs/add/operator/map';
 */
 @Injectable()
 export class GithubUsers {
+  //add users
+  githubUsers: any = null;
+  
+  constructor(public http: Http) {}
 
-  constructor(private http: Http) {}
+ load() {
+    if (this.githubUsers) {
+      // already loaded users
+      return Promise.resolve(this.githubUsers);
+    }
+
+    // don't have the users yet
+    return new Promise(resolve => {
+      // We're using Angular Http provider to request the users,
+      // then on the response it'll map the JSON data to a parsed JS object.
+      // Next we process the users and resolve the promise with the new data.
+      this.http.get('https://api.github.com/users')
+        .map(res => <Array<User>>(res.json()))
+        .subscribe(users => {
+          // we've got back the raw users, now generate the core schedule users
+          // and save the users for later reference
+          this.githubUsers = users;
+          resolve(this.githubUsers);
+        });
+    });
+  }
 
 }
+
 
